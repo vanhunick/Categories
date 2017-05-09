e14c6aadcfdcf26c6b8bcca2d8bc02030eeaa381
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-05-09 20:26:24 +1200

    Cleaned up index module and added output for number of results found

--- js/indexModule.js
+++ js/indexModule.js
@@ -1,22 +1,22 @@
 var IndexModule = (function(){
 
-
   //Cache elements
   var $searchButton = $('#searchButton');
   var $searchBar = $('#searchBar');
   var $output = $('#output');
 
+  // Used to copy code to clipboard
   var clipboard = null;
 
   // Setup listeners
-  $searchButton.click(function(){
-      var val = $('#search-friends').val();
+  $searchButton.click(() => {
      var data = Data.search($searchBar.val());
      insertResults(data);
   });
 
+
   // Respond to enter being pressed in search bar
-  $searchBar.keypress(function(event){
+  $searchBar.keypress((event) => {
    var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
       var data = Data.search($searchBar.val());
@@ -26,10 +26,17 @@ var IndexModule = (function(){
 
 
    var insertResults = function (data) {
-     $output.html('');
+     $output.html(''); // Clear prev results
+
+     // Display amount of results 
+     if(data.length === 0){
+        $output.html('<h4 class="center">No results</h4>');
+     } else {
+      $output.html('<h4 class="center">Your search returned '+ data.length +' results</h4>');
+     }
 
       var id = 0;
-      data.forEach(function(d){
+      data.forEach((d) => {
           insertElement(d, 'R'+id);
           id++;
       });
@@ -48,7 +55,7 @@ var IndexModule = (function(){
 
      var classDescriptionHTML = '<h4 class="heading"><b>'+element.class.description+'</b></h4>';
      var groupHTML = "<h5>"+element.group.description+ " " + element.division.code +""+ element.group.code +"000</h5>"
-     var subDivisionHTML = "<h5>"+element.subdivision.description+ " " + element.division.code +""+ element.subdivision.code +"000</h5>"
+     var subDivisionHTML = "<h5>"+element.subdivision.description+ " " + element.division.code +""+ element.subdivision.code +"0000</h5>"
 
      $output.append(
        '<div class="row"><div class="output">'+classDescriptionHTML+'<h4>'+matches+'</h4><h5 class="code">Code <span value="bob" id="'+id+'"><b>'+element.code+'</b></span></h5><button  type="button" class="btn copy-code" data-clipboard-target="#'+id+'">Copy Code</button>'+groupHTML+' '+subDivisionHTML+'<hr></div></div>'
